fba505a4195be7b3d9f91b8e3d825173510b5426
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-01 11:50:26 +0800

    fix: callback endpoint request

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import getTokenRoute from './routes/getTokenRoute.js';
-import getCurrentlyPlayingRoute from './routes/getCurrentPlayingRoute.js';
+import getCurrentlyPlayingRoute from './routes/getCurrentlyPlayingRoute.js';
 import authRoute from './routes/authRoute.js';
 import callbackRoute from './routes/callbackRoute.js';
 
