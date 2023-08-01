290d8c37ae79f3419c78b040962adb3303898b04
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-01 13:45:06 +0800

    minor improvements

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Routes
 import getTokenRoute from './routes/getTokenRoute.js';
 import getCurrentlyPlayingRoute from './routes/getCurrentlyPlayingRoute.js';
 import authRoute from './routes/authRoute.js';
 import callbackRoute from './routes/callbackRoute.js';
 
-import testRoute from './routes/testRoute.js';
+// Clients
+import renderCurrentlyPlaying from './routes/embed/currently-playing.js';
 
 dotenv.config();
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the 'public' folder
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // Middleware
 app.use(express.json());
@@ -18,6 +29,5 @@ app.use(getTokenRoute);
 app.use(getCurrentlyPlayingRoute);
 app.use(authRoute);
 app.use(callbackRoute);
-app.use(testRoute);
 
 export default app;
