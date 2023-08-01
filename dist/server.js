e7d7abbcd274fdf45d89ad920dd782e8a0dfa88b
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-01 08:54:28 +0800

    feat(api): add endpoint for current music

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 const app = express();
 const port = 8000;
 app.listen(port, () => {
@@ -6,4 +7,23 @@ app.listen(port, () => {
 });
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.get('/current-music', async (req, res) => {
+    try {
+        const token = req.query.token;
+        if (!token) {
+            return res.status(400).json({ error: 'API token is missing.' });
+        }
+        const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        const musicData = response.data;
+        return res.status(200).json(musicData);
+    }
+    catch (error) {
+        console.error("Error fetching current music: ", error);
+        return res.status(500).json({ error: 'An error occured while fetching current music.' });
+    }
 });
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import axios from 'axios';
 
 const app: Express = express();
 const port: number = 8000;
@@ -9,4 +10,27 @@ app.listen(port, () => {
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
+});
+
+app.get('/current-music', async (req: Request, res: Response) => {
+    try {
+        const token = req.query.token as string;
+        if (!token) {
+            return res.status(400).json({ error: 'API token is missing.' });
+        }
+
+        // Fetch the current playing music data from Spotify API
+        const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+
+        const musicData = response.data;
+
+        return res.status(200).json(musicData);
+    } catch (error) {
+        console.error("Error fetching current music: ", error);
+        return res.status(500).json({ error: 'An error occured while fetching current music.'})
+    }
 });
