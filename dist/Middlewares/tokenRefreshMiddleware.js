ef590d516f5c9d4c042d93636ce4d675cd4410fa
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-02 21:26:10 +0800

    chore: clean code in token refresh middleware

--- dist/Middlewares/tokenRefreshMiddleware.js
+++ dist/Middlewares/tokenRefreshMiddleware.js
@@ -3,6 +3,21 @@ import { SPOTIFY_ACCOUNTS_API_BASE_URL, CLIENT_ID, CLIENT_SECRET } from '../util
 import { getUserFromCollection, updateUserAccessToken } from '../database/user.js';
 import { isAccessTokenExpired } from '../utils/helpers.js';
 import { stringify } from 'querystring';
+async function refreshAccessToken(user) {
+    const data = stringify({
+        grant_type: 'refresh_token',
+        refresh_token: user.refresh_token,
+    });
+    const authorization = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+    const response = await axios.post(`${SPOTIFY_ACCOUNTS_API_BASE_URL}/api/token`, data, {
+        headers: {
+            'Authorization': `Basic ${authorization}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    });
+    const { access_token } = response.data;
+    return access_token;
+}
 export const tokenRefreshMiddleware = async (req, res, next) => {
     try {
         const key = req.query.key;
@@ -17,20 +32,9 @@ export const tokenRefreshMiddleware = async (req, res, next) => {
             return res.status(404).json({ error: 'Creation date not found.' });
         }
         if (isAccessTokenExpired(user.created_at, user.expires_in)) {
-            const data = stringify({
-                grant_type: 'refresh_token',
-                refresh_token: user.refresh_token,
-            });
-            const authorization = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
-            const response = await axios.post(`${SPOTIFY_ACCOUNTS_API_BASE_URL}/api/token`, data, {
-                headers: {
-                    'Authorization': `Basic ${authorization}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                }
-            });
-            const { access_token } = response.data;
-            await updateUserAccessToken(key, access_token);
-            req.headers.authorization = `Bearer ${access_token}`;
+            const newAccessToken = await refreshAccessToken(user);
+            await updateUserAccessToken(key, newAccessToken);
+            req.headers.authorization = `Bearer ${newAccessToken}`;
         }
         next();
     }
--- src/Middlewares/tokenRefreshMiddleware.ts
+++ src/Middlewares/tokenRefreshMiddleware.ts
@@ -1,9 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 import { SPOTIFY_ACCOUNTS_API_BASE_URL, CLIENT_ID, CLIENT_SECRET } from '../utils/constants.js';
-import { getUserFromCollection, updateUserAccessToken } from '../database/user.js';
+import { UserInfo, getUserFromCollection, updateUserAccessToken } from '../database/user.js';
 import { isAccessTokenExpired } from '../utils/helpers.js';
 import { stringify } from 'querystring';
+
+async function refreshAccessToken(user: UserInfo) {
+    const data = stringify({
+        grant_type: 'refresh_token',
+        refresh_token: user.refresh_token,
+    });
+
+    const authorization = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+
+    const response = await axios.post(`${SPOTIFY_ACCOUNTS_API_BASE_URL}/api/token`, data, {
+        headers: {
+            'Authorization': `Basic ${authorization}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    });
+
+    const { access_token } = response.data;
+    return access_token;
+}
 
 export const tokenRefreshMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -12,39 +31,20 @@ export const tokenRefreshMiddleware = async (req: Request, res: Response, next: 
             return res.status(400).json({ error: 'API key is missing.' });
         }
 
-        // Get the user from the database based on the key (username, ID, or any unique identifier)
         const user = await getUserFromCollection(key);
         if (!user) {
             return res.status(404).json({ error: 'User not found.' });
         }
-        if (!user.created_at){
-            return res.status(404).json({ error: 'Creation date not found.'});
+        if (!user.created_at) {
+            return res.status(404).json({ error: 'Creation date not found.' });
         }
 
         if (isAccessTokenExpired(user.created_at, user.expires_in)) {
-            const data = stringify({
-                grant_type: 'refresh_token',
-                refresh_token: user.refresh_token,
-            });
-            
-            const authorization = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
-
-            // Access token has expired, so request a new access token using the refresh token
-            const response = await axios.post(`${SPOTIFY_ACCOUNTS_API_BASE_URL}/api/token`, data, {
-                headers: {
-                    'Authorization' : `Basic ${authorization}`,
-                    'Content-Type' : 'application/x-www-form-urlencoded',
-                }});
-
-            // Update the user's access token and expiry time in the database
-            const { access_token } = response.data;
-            await updateUserAccessToken(key, access_token);
-
-            // Set the updated access token in the request headers
-            req.headers.authorization = `Bearer ${access_token}`;
+            const newAccessToken = await refreshAccessToken(user);
+            await updateUserAccessToken(key, newAccessToken);
+            req.headers.authorization = `Bearer ${newAccessToken}`;
         }
 
-        // Token is either valid or refreshed, proceed to the next middleware or route handler
         next();
     } catch (error) {
         console.error('Error refreshing token:', error);
