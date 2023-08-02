067707aa71483d3fc68aa4f888e12532580d5c90
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-03 00:04:30 +0800

    refactor: render nothingplaying if no spotify data

--- src/routes/embed/currentlyPlaying.ts
+++ src/routes/embed/currentlyPlaying.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
 import { renderCurrentlyPlaying } from '../../client/renderCurrentlyPlaying.js';
 import { fetchCurrentMusicData } from '../../middlewares/fetchCurrentMusicData.js';
+import { renderNothingPlaying } from '../../client/renderNothingPlaying.js';
 
 const router = express.Router();
 
 router.get('/embed/currently-playing', fetchCurrentMusicData, (req: Request, res: Response) => {
     const musicData = req.musicData;
-    return res.send(renderCurrentlyPlaying(musicData));
+    if (musicData) return res.send(renderCurrentlyPlaying(musicData))
+    else return res.send(renderNothingPlaying());
 });
 
 export default router;
